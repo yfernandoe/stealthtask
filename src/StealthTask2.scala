4f02623f0802a334e4a408ac20193296920bcee2
fernandoy <yogesh.fernando@gmail.com>
2017-06-05 23:08:09 +0100

    dded offer realted changes in Test2 and its related scalatests.

--- src/StealthTask2.scala
+++ src/StealthTask2.scala
@@ -5,8 +5,12 @@ class StealthTask2 {
 
   val itemDetail = Map[String, Double]("Cola"->0.50, "Coffee"->1, "Cheese Sandwich"-> 2.00, "Steak Sandwich"-> 4.50)
 
+  val itemType = Map[String, String]("Cola"->"Cold", "Coffee"->"Hot", "Cheese Sandwich"->"Cold", "Steak Sandwich"->"Hot")
+  val itemFoodType = Map[String, String]("Cola"->"Drink", "Coffee"->"Drink", "Cheese Sandwich"->"Food", "Steak Sandwich"->"Food")
+
 
   var totalCost = 0.0;
+  var serviceCharge = 0.0
 
 
   def countItems(aList: List[String]):scala.collection.mutable.Map[String, Int] = {
@@ -42,6 +46,63 @@ class StealthTask2 {
 
     totalCost = total
   }
+
+
+  def calculateAll( order :  List[String]) = {
+    val itemMap = countItems(order)
+
+    var total = 0.0
+    var serviceCh = 0.0
+    var hotFoodService = false
+    var foodService = false
+    for ((prod, count) <- itemMap){
+      val tmp = count * ((itemDetail.get(prod)) match {
+        case Some(x:Double) => x
+        case _ => Double.MinValue
+      })
+
+
+      if ((((itemFoodType.get(prod)) match {
+        case Some(x:String) => x
+        case _ => ""
+      }) == "Food") && (!foodService)){
+        foodService = true
+        if ((((itemType.get(prod)) match {
+          case Some(x:String) => x
+          case _ => ""
+        }) == "Hot") ){
+          hotFoodService = true
+        }
+      }
+      total = total +tmp
+    }
+    if (hotFoodService) {
+      serviceCh = ((total/100) *20)
+
+    } else if(foodService ) {
+      serviceCh = ((total/100) *10)
+
+    }
+
+    serviceCh = Math.round(serviceCh * 100.0) / 100.0
+
+
+
+
+
+    if(serviceCh > 20.0 )
+      serviceCh = 20.0
+
+    total= total+serviceCh
+
+
+    println("total : "+total)
+    serviceCharge = serviceCh
+    totalCost = total
+  }
+
+
+
 
 
 
--- testsrc/StealthTask2Test.scala
+++ testsrc/StealthTask2Test.scala
@@ -18,10 +18,48 @@ class StealthTask2Test extends org.scalatest.FunSuite {
     assert(calcc.totalCost == 0.5)
   }
 
-  test("Calculate"){
+  test("Calculate only drinks"){
     val order  = List("Cola","Coffee","Cola","Coffee","Cola","Coffee","Cola","Coffee","Cola","Coffee","Cola","Coffee","Cola","Coffee")
     calcc.calculate(order)
     assert(calcc.totalCost == 10.5)
+  }
+
+
+
+
+  test("CalculateWith Service"){
+    val order  = List("Cola","Cheese Sandwich","Cola","Cheese Sandwich","Cola","Cheese Sandwich","Cola","Cheese Sandwich","Cola","Cheese Sandwich","Cola","Cheese Sandwich")
+    calcc.calculateAll(order)
+    assert(calcc.totalCost == 16.5)
+  }
+
+
+
+
+  test("Calculate With Service for hot food"){
+    val order  = List("Cola","Cheese Sandwich", "Steak Sandwich","Cola","Cheese Sandwich","Cola","Cheese Sandwich", "Steak Sandwich","Cola", "Steak Sandwich","Cheese Sandwich","Cola", "Steak Sandwich","Cheese Sandwich","Cola","Cheese Sandwich")
+    calcc.calculateAll(order)
+    assert(calcc.serviceCharge == 6.60)
+  }
+
+  test("Calculate With Service > 20"){
+    val order  = List("Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich",
+      "Steak Sandwich","Steak Sandwich","Steak Sandwich","Steak Sandwich"
+    )
+    calcc.calculateAll(order)
+    assert(calcc.serviceCharge == 20.0)
   }
 
 
